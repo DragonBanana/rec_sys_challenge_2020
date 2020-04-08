72c25b63352bbd624476b6e8384dad76826b1dfd
DragonBanana <10493858@Polimi.it>
2020-04-08 15:27:22 +0200

    Sparse matrixes
    Sparse matrixes

---
+++ Utils/Data/Sparse/CSR_SparseMatrix.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+from abc import ABC
+import scipy.sparse as sps
+import RootPath
+import pathlib as pl
+
+
+class CSR_SparseMatrix(ABC):
+    ROOT_PATH = RootPath.get_root().joinpath("Dataset/Sparse/")
+
+    def __init__(self, matrix_name: str):
+        self.matrix_name = matrix_name
+        self.path = pl.Path(f"{CSR_SparseMatrix.ROOT_PATH}/sparse/{self.matrix_name}.npz")
+
+    def has_matrix(self):
+        return self.path.is_file()
+
+    def load_matrix(self):
+        assert self.has_matrix(), f"The Matrix {self.matrix_name} does not exists. Create it first."
+        matrix = sps.load_npz(self.path)
+        return matrix
+
+    @abstractmethod
+    def create_matrix(self):
+        pass
+
+    def load_or_create(self):
+        if not self.has_matrix():
+            self.create_matrix()
+        return self.load_matrix()
+
+    def save_matrix(self, matrix):
+        self.path.parent.mkdir(parents=True, exist_ok=True)
+        sps.save_npz(self.path, matrix)
