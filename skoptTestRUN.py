ab08779f1953517d2a95bc1b6f24472baa008953
DragonBanana <10493858@Polimi.it>
2020-04-08 15:30:38 +0200

    Added X and Y in skoptTestRun
    Added X and Y in skoptTestRun

--- skoptTestRUN.py
+++ skoptTestRUN.py
@@ -10,7 +10,7 @@ import time
 import datetime as dt
 from ParamTuning.ModelInterface import ModelInterface
 from ParamTuning.Optimizer import Optimizer
-
+from Utils.Data import Data
 
 
 def main():
@@ -23,6 +23,41 @@ def main():
     OP.setParameters(n_calls=3, n_random_starts=3)
     #OP.loadTrainData(X_train, Y_train)
     #OP.loadTestData(X_test, Y_test)
+
+    # Defining the dataset used
+    train_dataset = "train_split_with_timestamp_from_train_random_seed_888_timestamp_threshold_1581465600_holdout_1"
+    test_dataset = "val_split_with_timestamp_from_train_random_seed_888_timestamp_threshold_1581465600_holdout_1"
+
+    # Define the X label
+    X_label = [
+        "raw_feature_creator_follower_count",
+        "raw_feature_creator_following_count",
+        "raw_feature_creator_is_verified",
+        "raw_feature_engager_follower_count",
+        "raw_feature_engager_following_count",
+        "raw_feature_engager_is_verified",
+        "tweet_feature_is_retweet",
+        "tweet_feature_is_quote",
+        "tweet_feature_is_top_level",
+        "raw_feature_engagement_creator_follows_engager"
+    ]
+    # Define the Y label
+    Y_label = [
+        "tweet_feature_engagement_is_retweet"
+    ]
+
+    # Load train data
+    loading_data_start_time = time.time()
+    X_train, Y_train = Data.get_dataset_xgb(train_dataset, X_label, Y_label)
+
+    # Load test data
+    X_test, Y_test = Data.get_dataset_xgb(test_dataset, X_label, Y_label)
+    print(f"Loading data time: {time.time() - loading_data_start_time} seconds")
+
+    OP = Optimizer(model_name, kind, make_log=True, make_save=True)
+    OP.setParameters(n_calls=1, n_random_starts=1)
+    OP.loadTrainData(X_train, Y_train)
+    OP.loadTestData(X_test, Y_test)
     res=OP.optimize()
 
     '''
