f1fb5bf3b37da7d3492cc997a952b219359d7aee
DragonBanana <10493858@Polimi.it>
2020-04-20 18:34:47 +0200

    Reducing the max-depth range
    Reducing the max-depth range

--- Utils/Base/ParamRangeDict.py
+++ Utils/Base/ParamRangeDict.py
@@ -29,7 +29,7 @@ from skopt.space import Categorical
 
 def xgbRange():
     param_range_dict = [Integer(5, 200),                    #num_rounds
-                        Integer(5, 200),                    #max_depth
+                        Integer(4, 64),                    #max_depth
                         Integer(1, 10),                    #min_child_weight
                         Real(0.3, 1),                       #colsample_bytree
                         Real(0.0001, 1, 'log-uniform'),     #learning rate
