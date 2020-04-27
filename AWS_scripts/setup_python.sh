eae5a11b9196e1fbcae11fe5d1e57d78b432fc8b
DragonBanana <10493858@Polimi.it>
2020-04-27 16:09:57 +0200

    Infrastructures and platforms

--- AWS_scripts/setup_python.sh
+++ AWS_scripts/setup_python.sh
@@ -1,5 +1,7 @@
 sudo apt install git -y
 sudo apt install screen -y
 sudo apt install python3-pip -y
+sudo apt install python3-venv -y
+source venv/bin/activate
 pip3 install -r requirements.txt	Models/GBM/LightGBM.py
-mv ./AWS_scripts/core.py
+mv ./AWS_scripts/core.py venv/lib/python3.6/site-packages/xgboost/core.py
