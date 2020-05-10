4e5be71d5594b7ffb5a637c6b40a3dc0045a7dea
Andrea Donati <andrewgifted@gmail.com>
2020-05-10 19:07:04 +0200

    holdout ready for generation

--- Utils/Data/Split/HoldoutSplit.py
+++ Utils/Data/Split/HoldoutSplit.py
@@ -9,7 +9,7 @@ STARTING_TIME = 1580947200
 SECONDS_IN_A_DAY = 86400
 SECONDS_IN_A_WEEK = SECONDS_IN_A_DAY * 7
 
-def holdout_split_train_test(input_dataset_id: str, pc_hold_out: float = 0.20, random_seed = 888):
+def holdout_split_train_test(input_dataset_id: str, pc_hold_out: float = 0.30, random_seed = 888):
     input_file_name = f"{RootPath.get_dataset_path()}/{input_dataset_id}.csv.gz"
     train_file_name = f"{RootPath.get_dataset_path()}/holdout/train.csv.gz"
     val_file_name = f"{RootPath.get_dataset_path()}/holdout/test.csv.gz"
@@ -23,10 +23,10 @@ def holdout_split_train_test(input_dataset_id: str, pc_hold_out: float = 0.20, r
     r.random()
     with gzip.open(input_file_name, "rb") as file:
         for line in file:
-            a, b, c, d, e, f, g, h, timestamp, j = line.decode('utf-8').split("\x01", 9)
-            if r.random() > pc_hold_out:
+            if r.random() < pc_hold_out:
                 train_file.write(line)
             else:
+                a, b, c, d, e, f, g, h, timestamp, j = line.decode('utf-8').split("\x01", 9)
                 timestamp = int(timestamp)
                 timestamp += SECONDS_IN_A_WEEK
                 line = codecs.encode(a+"\x01"+b+"\x01"+c+"\x01"+d+"\x01"+e+"\x01"+f+"\x01"+g+"\x01"+h+"\x01"+str(timestamp)+"\x01"+j,encoding='utf-8')
