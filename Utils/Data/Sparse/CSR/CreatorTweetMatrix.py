c449e198ea598c091987ea70036df7ad5718f452
ciamir51 <nicolo.felicioni@mail.polimi.it>
2020-05-10 18:50:24 +0200

    sparse matrices now take in input a dataframe

--- Utils/Data/Sparse/CSR/CreatorTweetMatrix.py
+++ Utils/Data/Sparse/CSR/CreatorTweetMatrix.py
@@ -2,13 +2,18 @@ from Utils.Data.Data import get_dataset
 from Utils.Data.Sparse.CSR_SparseMatrix import CSR_SparseMatrix
 import numpy as np
 import scipy.sparse as sps
-
+import pandas as pd
 
 class CreatorTweetMatrix(CSR_SparseMatrix):
 
-    def __init__(self, dataset_id: str, max_user_id, max_tweet_id):
+    def __init__(self, df: pd.DataFrame, max_user_id, max_tweet_id):
         super().__init__("creator_tweet_csr_matrix")
-        self.dataset_id = dataset_id
+
+        assert df.columns.shape[0] == 2, "The dataframe must have exactly two columns"
+        assert 'mapped_feature_creator_id' in df.columns, "The dataframe must have mapped_feature_creator_id column"
+        assert 'mapped_feature_tweet_id' in df.columns, "The dataframe must have mapped_feature_tweet_id column"
+
+        self.df = df
         self.max_user_id = max_user_id
         self.max_tweet_id = max_tweet_id
 
@@ -16,19 +21,11 @@ class CreatorTweetMatrix(CSR_SparseMatrix):
     def create_matrix(self):
 
         # creation of the creator - tweet matrix
-        features_creator = [
-            "mapped_feature_tweet_id",
-            "mapped_feature_creator_id",
-            "tweet_feature_engagement_is_positive"
-        ]
-
-        df = get_dataset(features=features_creator, dataset_id=self.dataset_id)
-        df = df.drop_duplicates()
-
+        self.df = self.df.drop_duplicates()
         # the matrix will be User X Tweet
-        creator_ids_arr = df['mapped_feature_creator_id'].values
-        tweet_ids_arr = df['mapped_feature_tweet_id'].values
-        creations_arr = np.array([1] * len(df))
+        creator_ids_arr = self.df['mapped_feature_creator_id'].values
+        tweet_ids_arr = self.df['mapped_feature_tweet_id'].values
+        creations_arr = np.array([1] * len(self.df))
 
         ctm = sps.coo_matrix((creations_arr, (creator_ids_arr, tweet_ids_arr)),
                              shape=(self.max_user_id+1, self.max_tweet_id+1)).tocsr()
--- Utils/Data/Sparse/CSR/URM.py
+++ Utils/Data/Sparse/CSR/URM.py
@@ -2,35 +2,35 @@ from Utils.Data.Data import get_dataset
 from Utils.Data.Sparse.CSR_SparseMatrix import CSR_SparseMatrix
 import scipy.sparse as sps
 import numpy as np
+import pandas as pd
 
 
 class URM(CSR_SparseMatrix):
 
-    def __init__(self, dataset_id: str, max_user_id, max_tweet_id):
+    def __init__(self, df: pd.DataFrame, max_user_id, max_tweet_id):
         super().__init__("urm_csr_matrix")
-        self.dataset_id = dataset_id
+
+        assert df.columns.shape[0] == 3, "The dataframe must have exactly three columns"
+        assert 'mapped_feature_engager_id' in df.columns, "The dataframe must have mapped_feature_engager_id column"
+        assert 'mapped_feature_tweet_id' in df.columns, "The dataframe must have mapped_feature_tweet_id column"
+        assert 'engagement' in df.columns, "The dataframe must have engagement column"
+
+        self.df = df
+
         self.max_user_id = max_user_id
         self.max_tweet_id = max_tweet_id
 
     def create_matrix(self):
         # creation of the urm
-
-        features_urm = [
-            "mapped_feature_tweet_id",
-            "mapped_feature_engager_id",
-            "tweet_feature_engagement_is_positive"
-        ]
-
-        df = get_dataset(features=features_urm, dataset_id=self.dataset_id)
-
         # taking only the positive interactions
         # it could be interesting to take also the negative with a -1 value
-        df = df[df['tweet_feature_engagement_is_positive'] == True]
+        self.df = self.df[self.df['tweet_feature_engagement_is_positive']==True]
+
 
         # the matrix will be User X Tweet
-        engager_ids_arr = df['mapped_feature_engager_id'].values
-        tweet_ids_arr = df['mapped_feature_tweet_id'].values
-        interactions_arr = np.array([1] * len(df))
+        engager_ids_arr = self.df['mapped_feature_engager_id'].values
+        tweet_ids_arr = self.df['mapped_feature_tweet_id'].values
+        interactions_arr = np.array([1] * len(self.df))
 
         urm = sps.coo_matrix((interactions_arr, (engager_ids_arr, tweet_ids_arr)),
                              shape=(self.max_user_id+1, self.max_tweet_id+1)).tocsr()
