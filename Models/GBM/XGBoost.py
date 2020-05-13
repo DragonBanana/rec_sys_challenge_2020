fcbbbe6adda2d8b4e1209702ad38b04f3b454c13
DragonBanana <10493858@Polimi.it>
2020-05-13 18:19:19 +0200

    Fixing load model

--- Models/GBM/XGBoost.py
+++ Models/GBM/XGBoost.py
@@ -196,7 +196,7 @@ class XGBoost(RecommenderGBM):
     # path: path to the model
     # -------------------------
     def load_model(self, path):
-        self.model = pickle.load(path, "rb")
+        self.model = pickle.load(open(path))
 
 
     #--------------------------------------------------
