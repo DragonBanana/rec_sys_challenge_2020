e862846be0b92ed6d5b0ee6bb566054cd02c09f7
LucaBartoccioni <bartoccioni-luca@libero.it>
2020-05-18 03:15:44 +0200

    fix

--- ParamTuning/ModelInterface.py
+++ ParamTuning/ModelInterface.py
@@ -916,13 +916,11 @@ class ModelInterface(object):
     def loadTrainData(self, X_train=None, Y_train=None, holder_train=None):
         self.X_train=X_train
         self.Y_train=Y_train
-        print("flag1")
         if holder_train is None:
             if self.model_name in "xgboost_classifier":
                 self.train = self.getDMat(X_train, Y_train)
 
             elif self.model_name in "catboost_classifier":
-                print("flag2")
                 self.train = self.getTrainPool(X_train, Y_train)
             '''
             #ADD IF WANT TO USE Dataset FROM LIGHTGBM
@@ -1195,8 +1193,11 @@ class ModelInterface(object):
 #--------------------------------------------------
     def getTrainPool(self, X, Y=None):
         #l = np.array(Y).astype(np.int32)
+        X = np.array(X)
+        Y = np.array(Y)
         print("Creating Pool:")
         return cat.Pool(X, label=Y, cat_features=self.categorical_features)
+        print("Pool created.")
 #--------------------------------------------------
 
 #--------------------------------------------------
@@ -1205,6 +1206,8 @@ class ModelInterface(object):
 #              Used in CatBoost
 #--------------------------------------------------
     def getPool(self, X, Y=None):
+        X = np.array(X)
+        Y = np.array(Y)
         if Y is not None:
             l = np.array(Y).astype(np.int32)
         else:
