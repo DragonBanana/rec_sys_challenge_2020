2dcce230e2198a22bc3b87915c55266dfd3b635f
LucaBartoccioni <bartoccioni-luca@libero.it>
2020-05-21 02:56:25 +0200

    fix

--- skoptTestRUN-CAT-like.py
+++ skoptTestRUN-CAT-like.py
@@ -231,7 +231,7 @@ def main():
                    y0=y)
 
     print("checkpoint 2")
-    OP.setParameters(n_calls=90, n_random_starts=35)
+    OP.setParameters(n_calls=50, n_random_starts=20)
     print("checkpoint 3")
     #Before introducing datasets put the categorical features otherwise they will be ignored.
     OP.setCategoricalFeatures([4,5,6,11,12,13,43,44,45,46,47])
