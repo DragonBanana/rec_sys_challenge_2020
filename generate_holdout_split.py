0ebba615b377b6b4505e3bcbc3c37d82e4399e4d
DragonBanana <10493858@Polimi.it>
2020-07-07 16:00:42 +0200

    cleaning repo

--- generate_holdout_split.py
+++
@@ -1,5 +0,0 @@
-from Utils.Data.DataUtils import create_all
-from Utils.Data.Split.HoldoutSplit import holdout_split_train_test
-
-if __name__ == '__main__':
-    holdout_split_train_test(input_dataset_id="train", pc_hold_out=0.80)
